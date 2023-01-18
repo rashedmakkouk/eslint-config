e603c912b34ee581a962cdbf2ba6f4096df693e9
Rashed Makkouk <rashed.makkouk@outlook.com>
2023-01-18 02:00:04 +0200

    feat: Refactor module implementation
    - Upgrade ECMAScript syntax support to '2022'
    - Add 'eslint-import-resolver-typescript' configuration
    - Add 'eslint-plugin-deprecation' configuration
    - Add '@typescript-eslint/recommended-requiring-type-checking' rules
    - Add '@typescript-eslint/strict' select rules
    - Update '@typescript-eslint/naming-convention' rule
    - Migrate 'Indent' rule to 'Prettier'
    - Add support for loading 'tsconfig.eslint.json'
    - Add support for loading 'tsconfig.json' files from 'monorepo' directory patterns
    - Enable rules previously set to 'off'
    - Add explanation and usage guidelines to configured rules
    - Update loaded 'plugins' priority
    - Reorder configuration properties
    - Extract reusable configuration values to variables
    - Update 'tsconfig.base.json' configuration in 'bases'

--- src/index.ts
+++ src/index.ts
@@ -1,157 +1,25 @@
+/* eslint-disable sort-keys */
+const RULE_ERROR = 'error';
+const RULE_OFF = 'off';
+const RULE_WARN = 'warn';
+
+const ecmaVersion = 2022;
+const project = [
+  './tsconfig?(.eslint).json',
+  './(packages|libs|apps)/*/tsconfig?(.eslint).json',
+  './docs/tsconfig?(.eslint).json',
+];
+const extensions = ['.ts', '.tsx', '.js', '.jsx'];
+const ignorePatterns = ['**/node_modules', '**/build', '**/dist'];
+
 const PRINT_WIDTH = 80;
+const TAB_WIDTH = 2;
 
 module.exports = {
-  env: {
-    es6: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:promise/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
-  ignorePatterns: [
-    '.eslintrc.*',
-    '**/node_modules',
-    '**/build',
-    '**/dist',
-    '**/logs',
-  ],
-  overrides: [
-    {
-      files: ['*'],
-      rules: {
-        '@typescript-eslint/ban-types': 'off',
-        '@typescript-eslint/camelcase': 'off',
-        '@typescript-eslint/explicit-member-accessibility': 'off',
-        '@typescript-eslint/indent': [0, 2],
-        // TODO: eslint v3 `https://github.com/typescript-eslint/typescript-eslint/releases/tag/v3.0.0`
-        '@typescript-eslint/naming-convention': [
-          'error',
-          // {
-          //   format: ['camelCase'],
-          //   selector: 'default',
-          // },
-          // {
-          //   custom: {
-          //     // TODO: Match `allowVariables` regex.
-          //     match: true,
-          //     regex: allowVariables.join('|'),
-          //   },
-          //   format: ['camelCase', 'snake_case'],
-          //   selector: 'variable',
-          // },
-          {
-            format: ['camelCase', 'UPPER_CASE', 'snake_case', 'PascalCase'],
-            leadingUnderscore: 'allow',
-            selector: 'variable',
-            trailingUnderscore: 'allow',
-          },
-          {
-            format: ['camelCase', 'snake_case'],
-            selector: 'parameter',
-          },
-          {
-            format: ['PascalCase'],
-            selector: 'typeLike',
-          },
-          {
-            format: ['PascalCase'],
-            selector: 'typeParameter',
-            suffix: ['T'],
-          },
-          // {
-          //   format: ['PascalCase'],
-          //   prefix: [
-          //     'allow',
-          //     'can',
-          //     'did',
-          //     'disable',
-          //     'enable',
-          //     'has',
-          //     'in',
-          //     'is',
-          //     'should',
-          //     'will',
-          //     'with',
-          //     'visible',
-          //   ],
-          //   selector: 'variable',
-          //   types: ['boolean'],
-          // },
-          {
-            format: ['PascalCase'],
-            selector: 'class',
-          },
-          {
-            format: ['PascalCase', 'camelCase'],
-            leadingUnderscore: 'allow',
-            selector: 'function',
-          },
-        ],
-        '@typescript-eslint/no-empty-interface': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-floating-promises': [
-          'warn',
-          { ignoreVoid: true },
-        ],
-        /** Use array destructuring [index, data] Object.entries(obj) instead. */
-        '@typescript-eslint/no-for-in-array': 'error',
-        '@typescript-eslint/no-inferrable-types': 'warn',
-        '@typescript-eslint/no-misused-promises': [
-          'error',
-          {
-            checksVoidReturn: false,
-          },
-        ],
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-call': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-        '@typescript-eslint/no-unsafe-return': 'off',
-        '@typescript-eslint/no-unused-vars': 'warn',
-        '@typescript-eslint/no-useless-constructor': 'error',
-        '@typescript-eslint/no-var-requires': 'warn',
-        '@typescript-eslint/prefer-nullish-coalescing': 'off',
-        '@typescript-eslint/prefer-optional-chain': 'warn',
-        '@typescript-eslint/restrict-template-expressions': 'off',
-        '@typescript-eslint/unbound-method': 'off',
-        'arrow-parens': ['warn', 'always'],
-        camelcase: 'off',
-        'import/named': 'off',
-        'import/namespace': 'off',
-        'import/no-unresolved': 'off',
-        'no-void': ['warn', { allowAsStatement: true }],
-        'prettier/prettier': [
-          'warn',
-          {
-            arrowParens: 'always',
-            bracketSpacing: true,
-            endOfLine: 'auto',
-            insertPragma: false,
-            printWidth: PRINT_WIDTH,
-            proseWrap: 'preserve',
-            requirePragma: false,
-            semi: true,
-            singleQuote: true,
-            tabWidth: 2,
-            trailingComma: 'es5',
-          },
-        ],
-        quotes: [
-          'warn',
-          'single',
-          { allowTemplateLiterals: false, avoidEscape: true },
-        ],
-        'react-hooks/exhaustive-deps': 'off',
-      },
-    },
-  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    sourceType: 'module',
+    ecmaVersion,
     ecmaFeatures: {
       arrowFunctions: true,
       blockBindings: true,
@@ -160,7 +28,6 @@ module.exports = {
       destructuring: true,
       forOf: true,
       generators: true,
-      jsx: false,
       module: true,
       objectLiteralComputedProperties: true,
       objectLiteralDuplicateProperties: true,
@@ -174,83 +41,293 @@ module.exports = {
       templateStrings: true,
       unicodeCodePointEscapes: true,
     },
-    ecmaVersion: 2021,
-    extensions: ['.ts', '.tsx', 'js', '.jsx', '.html'],
-    project: './tsconfig.json',
-    sourceType: 'module',
+    extensions: [...extensions, '.html'],
+    project,
   },
   plugins: [
     '@typescript-eslint',
     'eslint-plugin-tsdoc',
     'simple-import-sort',
     'import',
+    'deprecation',
   ],
+  extends: [
+    'eslint:recommended',
+    'plugin:promise/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    /** {@link https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended.ts} */
+    'plugin:@typescript-eslint/recommended',
+    /** {@link https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended-requiring-type-checking.ts} */
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    /** {@link https://github.com/prettier/eslint-plugin-prettier#recommended-configuration | Recommended Configuration} */
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    es6: true,
+    node: true,
+  },
+  ignorePatterns,
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': extensions,
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', 'jsx'],
+      },
+      typescript: {
+        /**
+         * Always try to resolve types under `<root>@types` directory even it
+         * doesn't contain any source code.
+         */
+        alwaysTryTypes: true,
+        project: [
+          'tsconfig.json',
+          '(packages|libs|apps)/*/tsconfig.json',
+          'docs/tsconfig.json',
+        ],
+      },
+    },
+  },
   rules: {
-    curly: ['error', 'all'],
-    'dot-notation': 'off',
-    'lines-around-comment': [
-      'off',
+    /** Avoid conflict with Prettier rule 'tabWidth'. */
+    indent: RULE_OFF,
+    /** Avoid conflict with Prettier rule 'tabWidth'. */
+    '@typescript-eslint/indent': RULE_OFF,
+    /**
+     * Base rule is disabled in favor of 'overrides' configuration to properly
+     * handle JS/TS codebase; see 'overrides' property.
+     */
+    '@typescript-eslint/explicit-function-return-type': RULE_OFF,
+    /**
+     * Disabled in favor of a more explicit and granular rule:
+     * {@link https://typescript-eslint.io/rules/naming-convention | @typescript-eslint/naming-convention}
+     */
+    camelcase: RULE_OFF,
+    // TODO: eslint v3 `https://github.com/typescript-eslint/typescript-eslint/releases/tag/v3.0.0`
+    '@typescript-eslint/naming-convention': [
+      RULE_ERROR,
       {
-        afterBlockComment: true,
-        afterLineComment: false,
-        allowArrayEnd: true,
-        allowArrayStart: true,
-        allowBlockEnd: true,
-        allowBlockStart: true,
-        allowObjectEnd: true,
-        allowObjectStart: true,
-        beforeBlockComment: true,
-        beforeLineComment: false,
+        format: ['camelCase', 'snake_case', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+        selector: 'variable',
+        trailingUnderscore: 'forbid',
+      },
+      {
+        format: null,
+        modifiers: ['destructured'],
+        selector: 'variable',
+      },
+      {
+        format: ['camelCase'],
+        leadingUnderscore: 'forbid',
+        selector: 'parameter',
+        trailingUnderscore: 'forbid',
+      },
+      {
+        format: ['StrictPascalCase'],
+        selector: 'typeLike',
+      },
+      {
+        custom: {
+          match: false,
+          regex: '^I[A-Z]',
+        },
+        format: ['StrictPascalCase'],
+        selector: 'interface',
+      },
+      {
+        format: ['StrictPascalCase'],
+        selector: 'typeParameter',
+        suffix: ['T'],
+      },
+      {
+        format: ['StrictPascalCase'],
+        leadingUnderscore: 'forbid',
+        selector: 'class',
+        trailingUnderscore: 'forbid',
+      },
+      {
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+        selector: 'function',
+        trailingUnderscore: 'forbid',
       },
     ],
+    /** Interface naming for brevity, may simply extend other interfaces. */
+    '@typescript-eslint/no-empty-interface': RULE_OFF,
+    '@typescript-eslint/no-explicit-any': [RULE_ERROR, { fixToUnknown: true }],
+    '@typescript-eslint/no-floating-promises': [
+      RULE_WARN,
+      { ignoreVoid: true },
+    ],
+    /**
+     * If you want to iterate over indices, see below for alternatives.
+     * {@link https://typescript-eslint.io/rules/no-for-in-array | no-for-in-array}
+     */
+    '@typescript-eslint/no-for-in-array': RULE_ERROR,
+    '@typescript-eslint/no-inferrable-types': RULE_WARN,
+    /**
+     * ESLint base rule is disabled in favor of:
+     * \@typescript-eslint/no-invalid-this.
+     */
+    'no-invalid-this': RULE_OFF,
+    '@typescript-eslint/no-invalid-this': RULE_ERROR,
+    '@typescript-eslint/no-misused-promises': [
+      RULE_ERROR,
+      { checksVoidReturn: false },
+    ],
+    /**
+     * ESLint base rule is disabled in favor of:
+     * \@typescript-eslint/no-throw-literal
+     */
+    'no-throw-literal': RULE_OFF,
+    '@typescript-eslint/no-throw-literal': [
+      RULE_ERROR,
+      {
+        allowThrowingAny: true,
+        allowThrowingUnknown: true,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': RULE_WARN,
+    '@typescript-eslint/no-useless-constructor': RULE_ERROR,
+    '@typescript-eslint/no-var-requires': RULE_ERROR,
+    '@typescript-eslint/prefer-for-of': RULE_ERROR,
+    '@typescript-eslint/prefer-includes': RULE_ERROR,
+    '@typescript-eslint/prefer-nullish-coalescing': RULE_WARN,
+    '@typescript-eslint/prefer-optional-chain': RULE_WARN,
+    '@typescript-eslint/prefer-string-starts-ends-with': RULE_ERROR,
+    '@typescript-eslint/prefer-ts-expect-error': RULE_ERROR,
+    '@typescript-eslint/promise-function-async': RULE_ERROR,
+    '@typescript-eslint/restrict-template-expressions': [
+      RULE_WARN,
+      {
+        allowBoolean: true,
+        allowNumber: true,
+      },
+    ],
+    '@typescript-eslint/unbound-method': [RULE_WARN, { ignoreStatic: true }],
+    'arrow-parens': [RULE_ERROR, 'always'],
+    curly: [RULE_ERROR, 'all'],
+    'deprecation/deprecation': RULE_WARN,
+    /** Avoid false positives when using Bracket notation. */
+    'dot-notation': RULE_OFF,
+    /** Suppress errors and warnings when importing from external packages. */
+    'import/named': RULE_OFF,
+    /** Suppress errors and warnings when importing from external packages. */
+    'import/namespace': RULE_OFF,
+    /** Suppress errors and warnings when importing from external packages. */
+    'import/no-named-as-default': RULE_OFF,
+    'import/no-unresolved': RULE_ERROR,
+    'lines-around-comment': [
+      RULE_WARN,
+      {
+        beforeBlockComment: false,
+        afterBlockComment: false,
+        beforeLineComment: false,
+        afterLineComment: false,
+        allowArrayStart: true,
+        allowArrayEnd: true,
+        allowBlockStart: true,
+        allowBlockEnd: true,
+        allowObjectStart: true,
+        allowObjectEnd: true,
+      },
+    ],
+
+    /** Line length: 80 */
     'max-len': [
-      'warn',
+      RULE_WARN,
       {
         code: PRINT_WIDTH,
-        ignorePattern: '^s*import|export*',
+        /**
+         * Ignores patterns:
+         * - Comment line used to disable ESLint rule.
+         * - Module imports and exports.
+         */
+        ignorePattern: '/* eslint-disable|^s*import|export*',
         ignoreUrls: true,
       },
     ],
-    'no-async-promise-executor': 'off',
-    'no-case-declarations': 'off',
-    'no-catch-shadow': 'warn',
-    'no-console': 'warn',
-    'no-mixed-operators': 'error',
-    'no-multiple-empty-lines': ['warn', { max: 1 }],
-    'no-prototype-builtins': 'off',
-    'prefer-const': 'warn',
-    'require-atomic-updates': 'off',
-    'require-await': 'warn',
-    'sort-imports': 'off',
-    'import/first': 'error',
-    'import/newline-after-import': 'off',
-    'import/no-duplicates': 'error',
-    'simple-import-sort/exports': 'warn',
-    'simple-import-sort/imports': 'warn',
+    'no-async-promise-executor': RULE_OFF,
+    'no-confusing-arrow': [RULE_ERROR, { allowParens: false }],
+    'no-console': RULE_WARN,
+    'no-mixed-operators': RULE_ERROR,
+    'no-multiple-empty-lines': [RULE_WARN, { max: 1 }],
+    'no-tabs': [RULE_ERROR, { allowIndentationTabs: true }],
+    'no-unexpected-multiline': RULE_ERROR,
+    'no-use-before-define': [
+      RULE_ERROR,
+      {
+        allowNamedExports: false,
+        classes: true,
+        functions: true,
+        variables: true,
+      },
+    ],
+    'no-var': RULE_ERROR,
+    'no-void': [RULE_WARN, { allowAsStatement: true }],
+    'object-shorthand': [RULE_ERROR, 'properties'],
+    'prefer-const': RULE_WARN,
+    'prefer-spread': RULE_WARN,
+    'prefer-template': RULE_ERROR,
+    'prettier/prettier': [
+      RULE_WARN,
+      {
+        bracketSpacing: true,
+        endOfLine: 'auto',
+        insertPragma: false,
+        printWidth: PRINT_WIDTH,
+        proseWrap: 'preserve',
+        /** {@link https://prettier.io/docs/en/options.html#require-pragma} */
+        requirePragma: false,
+        semi: true,
+        singleQuote: true,
+        /** Indent formatting. */
+        tabWidth: TAB_WIDTH,
+        trailingComma: 'es5',
+      },
+    ],
+    quotes: [
+      RULE_WARN,
+      'single',
+      {
+        allowTemplateLiterals: false,
+        avoidEscape: true,
+      },
+    ],
+    'quote-props': [RULE_ERROR, 'as-needed'],
+    /**
+     * Raises sorting issues when used with 'eslint-plugin-simple-import-sort'.
+     * Turned off in favor of import/export rules configured below.
+     *
+     */
+    'sort-imports': RULE_OFF,
+    'simple-import-sort/imports': RULE_WARN,
+    'simple-import-sort/exports': RULE_WARN,
+    'import/first': RULE_ERROR,
+    'import/newline-after-import': RULE_WARN,
+    'import/no-duplicates': RULE_ERROR,
     'sort-keys': [
-      'warn',
+      RULE_WARN,
       'asc',
       {
         caseSensitive: true,
         natural: false,
       },
     ],
-    'sort-vars': [
-      'warn',
-      {
-        ignoreCase: false,
-      },
-    ],
-    'tsdoc/syntax': 'warn',
+    'sort-vars': [RULE_WARN, { ignoreCase: false }],
+    'spaced-comment': ['error', 'always', { exceptions: ['-', '+'] }],
+    'tsdoc/syntax': RULE_WARN,
   },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', 'jsx'],
+  overrides: [
+    {
+      /** Enable the rule specifically for TypeScript files. */
+      files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': RULE_ERROR,
       },
     },
-  },
+  ],
 };
