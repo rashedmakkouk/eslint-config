a95ea1f7d331fda639f38f890816e494ad40591d
Rashed Makkouk <rashed.makkouk@outlook.com>
2023-01-26 01:44:19 +0200

    chore: Drop 'parserOptions.project' Node Glob pattern in favor of explicit paths (#4)

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,25 @@ const RULE_OFF = 'off';
 const RULE_WARN = 'warn';
 
 const ecmaVersion = 2022;
-const project = [
-  './tsconfig?(.eslint).json',
-  './(packages|libs|apps)/*/tsconfig?(.eslint).json',
-  './docs/tsconfig?(.eslint).json',
+/**
+ * Dropped 'parserOptions.project' Node Glob pattern for explicit paths.
+ * {@link https://github.com/isaacs/node-glob/blob/f5a57d3d6e19b324522a3fa5bdd5075fd1aa79d1/README.md#glob-primer | Node Glob}
+ *
+ * See Issue #4.
+ */
+const projectDefaultPaths = [
+  './tsconfig.json',
+  './apps/*/tsconfig.json',
+  './libs/*/tsconfig.json',
+  './packages/*/tsconfig.json',
+  './docs/tsconfig.json',
+];
+const projectEslintPaths = [
+  './tsconfig.eslint.json',
+  './apps/*/tsconfig.eslint.json',
+  './libs/*/tsconfig.eslint.json',
+  './packages/*/tsconfig.eslint.json',
+  './docs/tsconfig.eslint.json',
 ];
 const extensions = ['.ts', '.tsx', '.js', '.jsx'];
 const ignorePatterns = ['**/node_modules', '**/build', '**/dist'];
@@ -42,7 +57,7 @@ module.exports = {
       unicodeCodePointEscapes: true,
     },
     extensions: [...extensions, '.html'],
-    project,
+    project: [...projectDefaultPaths, ...projectEslintPaths],
   },
   plugins: [
     '@typescript-eslint',
@@ -82,11 +97,7 @@ module.exports = {
          * doesn't contain any source code.
          */
         alwaysTryTypes: true,
-        project: [
-          'tsconfig.json',
-          '(packages|libs|apps)/*/tsconfig.json',
-          'docs/tsconfig.json',
-        ],
+        project: [...projectDefaultPaths],
       },
     },
   },
